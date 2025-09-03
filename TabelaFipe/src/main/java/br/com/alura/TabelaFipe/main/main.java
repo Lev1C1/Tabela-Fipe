0b7a6d81d0acc01836119af798d6e0c92b04b84a
Lev1C1 <levi.cirino1006@gmail.com>
2025-09-03 19:07:16 -0300

    Projeto TabelaFipe Start

---
+++ TabelaFipe/src/main/java/br/com/alura/TabelaFipe/main/main.java
@@ -0,0 +1,38 @@
+package br.com.alura.TabelaFipe.main;
+
+import br.com.alura.TabelaFipe.service.ConsumoAPI;
+
+import java.util.Scanner;
+
+public class Main {
+    private Scanner scanner = new Scanner(System.in);
+    private ConsumoAPI consumo = new ConsumoAPI();
+
+    private final String URL_BASE = "https://parallelum.com.br/fipe/api/v1/";
+
+    public void exibeMenu(){
+        var menu = """
+                *** OPÇÕES ***
+                Carro
+                Moto
+                Caminhão
+                
+                Digite uma das opções para consultar: 
+                """;
+        System.out.println(menu);
+        var opcao = scanner.nextLine();
+        String endereco;
+
+        if (opcao.toLowerCase().contains("carr")){
+            endereco = URL_BASE + "carros/marcas";
+        } else if (opcao.toLowerCase().contains("mot")){
+            endereco = URL_BASE + "motos/marcas";
+        } else {
+            endereco = URL_BASE + "caminhoes/marcas";
+        }
+
+        var json = consumo .obterDados(endereco);
+        System.out.println(json);
+
+    }
+}
